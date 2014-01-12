3c5fd926ba3381ad394e3765aed8d46927c1ab4a
Jack Szwergold <JackSzwergold@gmail.com>
2014-01-12 13:15:54 -0500

    Adding a static HTML example.

--- index.php
+++ index.php
@@ -71,10 +71,8 @@ $final_covers = implode('', $covers);
 
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
    . '<html xmlns="http://www.w3.org/1999/xhtml">'
-   . '<html lang="en">'
    . '<head>'
 
-   . '<meta charset="utf-8" />'
    . '<title></title>'
    . '<meta http-equiv="content-type" content="text/html; charset=utf-8" />'
    . '<meta name="description" content="" />'
