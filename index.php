2402086f06ee001536e890fbc87337422d1b5eaf
Jack Szwergold <JackSzwergold@gmail.com>
2015-11-10 22:49:37 -0500

    Adding Favicon stuff.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
  *
  * You should have received a copy of the license along with this
- * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>. 
+ * work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
  *
  * w: http://www.preworn.com
  * e: me@preworn.com
