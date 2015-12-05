532646227065dc30b50cb16a45e029b976293e28
Jack Szwergold <JackSzwergold@gmail.com>
2015-12-04 23:49:37 -0500

    Refactoring the frontend display helper stuff into a function.

--- lib/frontendDisplayHelpers.php
+++ lib/frontendDisplayHelpers.php
@@ -24,146 +24,160 @@
 require_once BASE_FILEPATH . '/lib/Mosaic.class.php';
 
 //**************************************************************************************//
-// Set an array of mode options.
+// Fetch the values out of the frontend display helper.
 
-$mode_options = array();
-
-$mode_options['micro']['width'] = 6;
-$mode_options['micro']['height'] = 6;
-$mode_options['micro']['block_size'] = 10;
-$mode_options['micro']['how_many'] = 25;
-
-$mode_options['tiny']['width'] = 12;
-$mode_options['tiny']['height'] = 12;
-$mode_options['tiny']['block_size'] = 10;
-$mode_options['tiny']['how_many'] = 16;
-
-$mode_options['small']['width'] = 23;
-$mode_options['small']['height'] = 23;
-$mode_options['small']['block_size'] = 10;
-$mode_options['small']['how_many'] = 9;
-
-$mode_options['large']['width'] = 46;
-$mode_options['large']['height'] = 46;
-$mode_options['large']['block_size'] = 10;
-$mode_options['large']['how_many'] = 3;
-
-$mode_options['mega']['width'] = 72;
-$mode_options['mega']['height'] = 72;
-$mode_options['mega']['block_size'] = 10;
-$mode_options['mega']['how_many'] = 1;
+list($VIEW_MODE, $body_content, $json_content) = frontend_display_helper();
 
 //**************************************************************************************//
-// Set the mode.
+// Define the frontend display helper function.
 
-if (FALSE) {
-  $mode_keys = array_keys($mode_options);
-  shuffle($mode_keys);
-  $VIEW_MODE = $mode_keys[0];
-}
-else {
-  $VIEW_MODE = 'large';
-}
+function frontend_display_helper () {
 
-//**************************************************************************************//
-// Set the image directory.
+	//**************************************************************************************//
+	// Set an array of mode options.
 
-$image_dir = 'images/';
+	$mode_options = array();
 
-//**************************************************************************************//
-// Check if there is an image directory. If not? Exit.
+	$mode_options['micro']['width'] = 6;
+	$mode_options['micro']['height'] = 6;
+	$mode_options['micro']['block_size'] = 10;
+	$mode_options['micro']['how_many'] = 25;
 
-if (!is_dir($image_dir)) {
-  die();
-}
+	$mode_options['tiny']['width'] = 12;
+	$mode_options['tiny']['height'] = 12;
+	$mode_options['tiny']['block_size'] = 10;
+	$mode_options['tiny']['how_many'] = 16;
 
-//**************************************************************************************//
-// Process the images in the directory.
+	$mode_options['small']['width'] = 23;
+	$mode_options['small']['height'] = 23;
+	$mode_options['small']['block_size'] = 10;
+	$mode_options['small']['how_many'] = 9;
 
-$skip_files = array('..', '.', '.DS_Store', 'ignore');
-$image_files = scandir($image_dir);
-$image_files = array_diff($image_files, $skip_files);
+	$mode_options['large']['width'] = 46;
+	$mode_options['large']['height'] = 46;
+	$mode_options['large']['block_size'] = 10;
+	$mode_options['large']['how_many'] = 3;
 
-if (empty($image_files)) {
-  die('Sorry. No images found.');
-}
+	$mode_options['mega']['width'] = 72;
+	$mode_options['mega']['height'] = 72;
+	$mode_options['mega']['block_size'] = 10;
+	$mode_options['mega']['how_many'] = 1;
 
-$raw_image_files = array();
-foreach ($image_files as $image_file_key => $image_file_value) {
-  $raw_image_files[$image_file_key] = $image_dir . $image_file_value;
-}
+	//**************************************************************************************//
+	// Set the mode.
 
-//**************************************************************************************//
-// Shuffle the image files.
+	if (FALSE) {
+	  $mode_keys = array_keys($mode_options);
+	  shuffle($mode_keys);
+	  $VIEW_MODE = $mode_keys[0];
+	}
+	else {
+	  $VIEW_MODE = 'large';
+	}
 
-shuffle($raw_image_files);
+	//**************************************************************************************//
+	// Set the image directory.
 
-//**************************************************************************************//
-// Slice off a subset of the image files.
+	$image_dir = 'images/';
 
-$image_files = array_slice($raw_image_files, 0, $mode_options[$VIEW_MODE]['how_many']);
+	//**************************************************************************************//
+	// Check if there is an image directory. If not? Exit.
 
-//**************************************************************************************//
-// Init the class and roll through the images.
+	if (!is_dir($image_dir)) {
+	  die();
+	}
 
-$MosaicClass = new ImageMosaic();
+	//**************************************************************************************//
+	// Process the images in the directory.
 
-// Init the items array.
-$items = array();
+	$skip_files = array('..', '.', '.DS_Store', 'ignore');
+	$image_files = scandir($image_dir);
+	$image_files = array_diff($image_files, $skip_files);
 
-// Loop through the image files array.
-foreach ($image_files as $image_file) {
+	if (empty($image_files)) {
+	  die('Sorry. No images found.');
+	}
 
-  // Set the options for the image processing.
-  $MosaicClass->set_image($image_file, $mode_options[$VIEW_MODE]['width'], $mode_options[$VIEW_MODE]['height'], $mode_options[$VIEW_MODE]['block_size']);
-  $MosaicClass->debug_mode(FALSE);
-  $MosaicClass->row_flip_horizontal(FALSE);
-  $MosaicClass->set_row_delimiter(NULL);
-  $MosaicClass->set_generate_images(TRUE);
-  $MosaicClass->set_overlay_image(TRUE);
+	$raw_image_files = array();
+	foreach ($image_files as $image_file_key => $image_file_value) {
+	  $raw_image_files[$image_file_key] = $image_dir . $image_file_value;
+	}
 
-  // Process the image and add it to the items array.
-  $processed_image = $MosaicClass->process_image();
-  $items[$image_file]['blocks'] = $processed_image['blocks'];
-  $items[$image_file]['json'] = $processed_image['json'];
+	//**************************************************************************************//
+	// Shuffle the image files.
 
-} // foreach
+	shuffle($raw_image_files);
 
-//**************************************************************************************//
-// Use 'array_filter' to filter out the empty images.
+	//**************************************************************************************//
+	// Slice off a subset of the image files.
 
-$items = array_filter($items);
+	$image_files = array_slice($raw_image_files, 0, $mode_options[$VIEW_MODE]['how_many']);
 
-//**************************************************************************************//
-// Place the images in <li> tags.
+	//**************************************************************************************//
+	// Init the class and roll through the images.
 
-// Init the image item and related json array.
-$image_item_array = $image_json_array = array();
+	$MosaicClass = new ImageMosaic();
 
-// Loop through the artworks array.
-foreach ($items as $file => $image) {
+	// Init the items array.
+	$items = array();
 
-  // Set the image item array value.
-  $image_item_array[$file] = sprintf('<li><div class="Padding">%s</div><!-- .Padding --></li>', $image['blocks']);
+	// Loop through the image files array.
+	foreach ($image_files as $image_file) {
 
-  // Set the image json array value.
-  $image_json_array[$file] = $image['json'];
+	  // Set the options for the image processing.
+	  $MosaicClass->set_image($image_file, $mode_options[$VIEW_MODE]['width'], $mode_options[$VIEW_MODE]['height'], $mode_options[$VIEW_MODE]['block_size']);
+	  $MosaicClass->debug_mode(FALSE);
+	  $MosaicClass->row_flip_horizontal(FALSE);
+	  $MosaicClass->set_row_delimiter(NULL);
+	  $MosaicClass->set_generate_images(TRUE);
+	  $MosaicClass->set_overlay_image(TRUE);
 
-} // foreach
+	  // Process the image and add it to the items array.
+	  $processed_image = $MosaicClass->process_image();
+	  $items[$image_file]['blocks'] = $processed_image['blocks'];
+	  $items[$image_file]['json'] = $processed_image['json'];
 
-// Set the body content.
-$body_content = sprintf('<ul>%s</ul>', implode('', $image_item_array));
+	} // foreach
 
-// Convert the JSON back to an object.
-$json_data_array = array();
-foreach ($image_json_array as  $image_json_value) {
-  $json_data_array[] = json_decode($image_json_value);
-}
+	//**************************************************************************************//
+	// Use 'array_filter' to filter out the empty images.
 
-// Now merge the JSON data object back into the parent image object.
-$image_object = $MosaicClass->build_image_object($json_data_array);
+	$items = array_filter($items);
 
-// Process the JSON content.
-$json_content = $MosaicClass->json_encode_helper($image_object);
+	//**************************************************************************************//
+	// Place the images in <li> tags.
+
+	// Init the image item and related json array.
+	$image_item_array = $image_json_array = array();
+
+	// Loop through the artworks array.
+	foreach ($items as $file => $image) {
+
+	  // Set the image item array value.
+	  $image_item_array[$file] = sprintf('<li><div class="Padding">%s</div><!-- .Padding --></li>', $image['blocks']);
+
+	  // Set the image json array value.
+	  $image_json_array[$file] = $image['json'];
+
+	} // foreach
+
+	// Set the body content.
+	$body_content = sprintf('<ul>%s</ul>', implode('', $image_item_array));
+
+	// Convert the JSON back to an object.
+	$json_data_array = array();
+	foreach ($image_json_array as  $image_json_value) {
+	  $json_data_array[] = json_decode($image_json_value);
+	}
+
+	// Now merge the JSON data object back into the parent image object.
+	$image_object = $MosaicClass->build_image_object($json_data_array);
+
+	// Process the JSON content.
+	$json_content = $MosaicClass->json_encode_helper($image_object);
+
+	return array($VIEW_MODE, $body_content, $json_content);
+
+} // frontend_display_helper
 
 ?>
