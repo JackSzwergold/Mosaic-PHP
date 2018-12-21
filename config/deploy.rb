ab62efa1c2535d7a2fb01fab5c0065fd1ac7d5d1
Jack Szwergold <JackSzwergold@gmail.com>
2018-12-20 20:46:19 -0500

    Bumping up Capistrano to verison 3.11.0 (June 03, 2018).

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.8.2'
+lock '3.11.0'
 
 set :application, 'mosaic'
 set :short_name, 'mosaic'
@@ -9,7 +9,7 @@ set :repo_url, 'git@github.com:JackSzwergold/Mosaic-PHP.git'
 set :deploy_to, '/var/www'
 
 # Default value for :scm is :git
-set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :pretty
 set :format, :pretty
