42ef1ec3d2a3967f3d60694f3fbc76835dd591e8
Jack Szwergold <JackSzwergold@gmail.com>
2025-12-12 13:30:22 -0500

    Refactoring and cleanup.

--- settings/conf.inc.php
+++ settings/conf.inc.php
@@ -33,11 +33,11 @@ $VALID_CHARSETS = array('utf-8','iso-8859-1','cp-1252');
 
 /**************************************************************************************************/
 // Site descriptive info.
-$SITE_TITLE = 'Mosaic';
-$SITE_DESCRIPTION = 'A dynamically generated image mosaic using PHP, the GD graphics library, HTML &amp; CSS.';
-$SITE_URL = 'http://www.szwergold.com/projects/mosaic/';
-$SITE_COPYRIGHT = '(c) Copyright ' . date('Y') . ' Jack Szwergold. Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.';
-$SITE_LICENSE_CODE = 'CC-BY-NC-SA-4.0';
+// $SITE_TITLE = 'Mosaic';
+// $SITE_DESCRIPTION = 'A dynamically generated image mosaic using PHP, the GD graphics library, HTML &amp; CSS.';
+// $SITE_URL = 'http://www.szwergold.com/projects/mosaic/';
+// $SITE_COPYRIGHT = '(c) Copyright ' . date('Y') . ' Jack Szwergold. Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.';
+// $SITE_LICENSE_CODE = 'CC-BY-NC-SA-4.0';
 $SITE_LICENSE = 'This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (CC-BY-NC-SA-4.0)';
 $SITE_ROBOTS = 'noindex, nofollow';
 $SITE_VIEWPORT = 'width=device-width, initial-scale=0.65, maximum-scale=2, minimum-scale=0.65, user-scalable=yes';
